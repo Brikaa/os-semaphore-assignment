f9362d0b593cd2bc0a7ab0ef8d5c82fc37cb5f49
Omar Brikaa <brikaaomar@gmail.com>
2022-11-12 22:38:32 +0200

    Write to file

--- primewriter/AfterConsumption.java
+++ primewriter/AfterConsumption.java
@@ -1,7 +1,21 @@
 package primewriter;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class AfterConsumption implements RunnableAction {
+    private FileWriter fileWriter;
+
+    public AfterConsumption(FileWriter fileWriter) {
+        this.fileWriter = fileWriter;
+    }
+
     public void run(int number) {
+        try {
+            fileWriter.write(number + " ");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
         System.out.println(number);
     }
 }
--- primewriter/App.java
+++ primewriter/App.java
@@ -1,13 +1,27 @@
 package primewriter;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Buffer buffer = new Buffer(8);
         long max = 100;
-        AfterConsumption action = new AfterConsumption();
+        FileWriter fw = new FileWriter("test.txt");
+
+        AfterConsumption action = new AfterConsumption(fw);
         PrimeProducer primeProducer = new PrimeProducer(buffer, max);
         PrimeConsumer primeConsumer = new PrimeConsumer(buffer, action);
+
         new Thread(primeProducer).start();
-        new Thread(primeConsumer).start();
+        Thread consumerThread = new Thread(primeConsumer);
+        consumerThread.start();
+        try {
+            consumerThread.join();
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+        System.out.println("Done");
+        fw.close();
     }
 }
