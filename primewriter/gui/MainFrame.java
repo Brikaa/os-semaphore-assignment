81944451faa9ff382009345f3a23ecab07db4bb6
Omar Brikaa <brikaaomar@gmail.com>
2022-11-15 18:26:53 +0200

    Double grid function

--- primewriter/gui/MainFrame.java
+++ primewriter/gui/MainFrame.java
@@ -1,12 +1,17 @@
 package primewriter.gui;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import java.awt.Container;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
@@ -32,33 +37,53 @@ public class MainFrame extends JFrame {
         this.startButton = new JButton("Start producer");
     }
 
+    private void doubleGrid(JPanel parent, LinkedList<JComponent> components) {
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets.left = 10;
+        c.insets.top = 5;
+        int size = components.size();
+        for (int i = 0; i < size; ++i) {
+            c.gridy = i;
+            for (int j = 0; j < 2; ++j) {
+                if (components.size() == 0) {
+                    break;
+                }
+                c.gridx = j;
+                JComponent component = components.remove();
+                parent.add(component, c);
+            }
+        }
+    }
+
     public void setVisible() {
         this.setTitle(APP_NAME);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container contentPane = this.getContentPane();
-        contentPane.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 0;
-        c.insets.left = 10;
-        c.insets.top = 5;
-        contentPane.add(new JLabel("Maximum number"), c);
-        c.gridx = 1;
-        contentPane.add(maximumNumberSpinner, c);
-        c.gridx = 0;
-        c.gridy = 1;
-        contentPane.add(new JLabel("Buffer size"), c);
-        c.gridx = 1;
-        contentPane.add(bufferSizeSpinner, c);
-        c.gridx = 0;
-        c.gridy = 2;
-        contentPane.add(new JLabel("Output file"), c);
-        c.gridx = 1;
-        contentPane.add(outputFileNameTextField, c);
-        c.gridx = 0;
-        c.gridy = 3;
-        contentPane.add(startButton, c);
+
+        JPanel inputPanel = new JPanel();
+        contentPane.add(inputPanel);
+        inputPanel.setLayout(new GridBagLayout());
+
+        doubleGrid(
+            inputPanel, new LinkedList<JComponent>(Arrays.asList(
+                new JLabel("Maximum number"),
+                maximumNumberSpinner,
+                new JLabel("Buffer size"),
+                bufferSizeSpinner,
+                new JLabel("Output file"),
+                outputFileNameTextField,
+                startButton
+            ))
+        );
+
+        // JPanel outputPanel = new JPanel();
+        // contentPane.add(outputPanel);
+        // outputPanel.setLayout(new GridBagLayout());
+        // GridBagConstraints outputPanelConstraints = new GridBagConstraints();
+
+
+
         this.setVisible(true);
     }
 }
