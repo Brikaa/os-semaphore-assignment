f5455a96947bc9f785be3ac5723e6594f8e854d5
Omar Brikaa <brikaaomar@gmail.com>
2022-11-15 17:09:35 +0200

    MainFrame ui elements

--- primewriter/gui/MainFrame.java
+++ primewriter/gui/MainFrame.java
@@ -4,9 +4,11 @@ import java.awt.Container;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JSpinner;
+import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 
 public class MainFrame extends JFrame {
@@ -15,6 +17,8 @@ public class MainFrame extends JFrame {
     private JLabel stopWatchLabel;
     private JSpinner maximumNumberSpinner;
     private JSpinner bufferSizeSpinner;
+    private JTextField outputFileNameTextField;
+    private JButton startButton;
     private static final String APP_NAME = "Prime writer";
 
     public MainFrame(JLabel lastPrimeLabel, JLabel counterLabel, JLabel stopWatchLabel) {
@@ -24,6 +28,8 @@ public class MainFrame extends JFrame {
         this.stopWatchLabel = stopWatchLabel;
         this.maximumNumberSpinner = new JSpinner(new SpinnerNumberModel(10000000, 2, 1000000000, 1));
         this.maximumNumberSpinner = new JSpinner(new SpinnerNumberModel(8, 2, 1000000000, 1));
+        this.outputFileNameTextField = new JTextField("primes.txt");
+        this.startButton = new JButton("Start producer");
     }
 
     public void setVisible() {
@@ -35,7 +41,8 @@ public class MainFrame extends JFrame {
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = 0;
-        c.ipadx = 10;
+        c.insets.left = 10;
+        c.insets.top = 5;
         contentPane.add(new JLabel("Maximum number"), c);
         c.gridx = 1;
         contentPane.add(maximumNumberSpinner, c);
@@ -44,6 +51,14 @@ public class MainFrame extends JFrame {
         contentPane.add(new JLabel("Buffer size"), c);
         c.gridx = 1;
         contentPane.add(counterLabel, c);
+        c.gridx = 0;
+        c.gridy = 2;
+        contentPane.add(new JLabel("Output file"), c);
+        c.gridx = 1;
+        contentPane.add(outputFileNameTextField, c);
+        c.gridx = 0;
+        c.gridy = 3;
+        contentPane.add(startButton, c);
         this.setVisible(true);
     }
 }
